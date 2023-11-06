e8823702e4f40a112fbbb34f5c06b063ff4c4a64
Tadas Petra <tadas@tadaspetra.com>
2023-11-06 14:26:24 -0800

    Using a nicer and more reliable grid layout

--- components/Call.tsx
+++ components/Call.tsx
@@ -44,34 +44,20 @@ function Videos(props: { channelName: string; AppID: string; }) {
   audioTracks.map((track) => track.play());
   const deviceLoading = isLoadingMic || isLoadingCam;
   if (deviceLoading) return <div className="flex flex-col items-center pt-40">Loading devices...</div>;
-
-  const numUsers = remoteUsers.length + 1;
-  let numCols = 1;
-  let numRows = 1;
-  switch (numUsers) {
-    case 1:
-      numCols = 1;
-      numRows = 1;
-      break;
-    case 2:
-      numCols = 2;
-      numRows = 1;
-      break;
-    case 3:
-      numCols = 3;
-      numRows = 1;
-      break;
-    case 4:
-      numCols = 2;
-      numRows = 2;
-      break;
-    default:
-      break;
-  }
+  const unit = 'minmax(0, 1fr) ';
 
   return (
     <div className="flex flex-col justify-between w-full h-screen p-1">
-      <div className={`grid grid-cols-${numCols} grid-rows-${numRows} gap-1 flex-1`}>
+      <div className={`grid  gap-1 flex-1`} style={{
+        gridTemplateColumns:
+          remoteUsers.length > 9
+            ? unit.repeat(4)
+            : remoteUsers.length > 4
+              ? unit.repeat(3)
+              : remoteUsers.length > 1
+                ? unit.repeat(2)
+                : unit
+      }}>
         <LocalVideoTrack track={localCameraTrack} play={true} className="w-full h-full" />
         {remoteUsers.map((user) => (
           <RemoteUser user={user} />
